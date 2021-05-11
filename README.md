fc336b65542f25fdec5217d3c4f6c77a60957319
Mostafa Mahmoud <61292945+MostafaAbbas-git@users.noreply.github.com>
2021-05-11 18:32:51 +0200

    makefile adjustment

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-# Microcontroller Task #2
------
-
-## Finished so far: 
-----------
-
-* Main.h
-* LED Module
-* Buttons Module
-
-## Unfinished:
------------
-
-* Port.h
-* Up & Down calling elevator Module
-* Pressing a floor's number and going to it Module
+# elevator-task
--- app.c
+++ app.c
@@ -11,7 +11,35 @@ void Delay_MS(unsigned int ms){
         }
     }
 }
+void Timer_init()
+{
+    TMOD = 0x01;
+    TL0 = 0x00;
+    TH0 = 0x4c;
+    TR0 = 1;
 
+}
+
+void Led(void) interrupt 1
+	
+{   
+    LED_Toggle(LED_1);
+    LED_Toggle(LED_2);
+    Delay_MS(400);
+    LED_Toggle(LED_1);
+    LED_Toggle(LED_2);
+    TR0 = 0;
+
+
+}
+
+void Buzeer(void) interrupt 0
+{
+   
+   TR0 = 0;
+   TR0 = 1;
+   
+}
 
 void main()
 {
@@ -26,12 +54,30 @@ void main()
     XBR1 = 0x016; // enable interrupt crossbar
     XBR2 = 0x40; // Cross bar enabled , weak Pull-up enabled
 
+    EA = 1; //enable global interrupt
+    EX0 = 1; // enable external interrupt 0
+    EX1 = 1 ;  //enable external interrupt 1
+    IT0 = 1;
+    IT1 = 1;
+
+    CKCON |= 0x00 ; //Select the system clock
     
 
-   
-    while(1)
-    {
+    ET0 = 1; //enable interrupt of timer0
 
-    }
+    Timer_init();
+    //ET1 = 1; //enable interrupt of timer1
+
+    
+
+    // TL1 = 0xfb;
+    // TH1 = 0xff;
+    LED_Init(LED_1,LED_OFF);
+    LED_Init(LED_2,LED_ON);
+    BUTTON_Init(BUTTON_OPEN);
+	// while (1){
+    //     LED_SetState(tLED led, tLED_State state);
+	// }
+while(1){}
 
 }
